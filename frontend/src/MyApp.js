f7bcb10cdd9374d74f41a387a2e71543ffc54e2a
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-04-12 12:56:37 -0700

    Changes Made

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -16,15 +16,15 @@ function MyApp() {
   }
 
   function updateList(person) {
-    makePostCall(person).then( result => {
-      if (result && result.status == 200)
-        setCharacters([...characters, person]);
-    });
+   makePostCall(person).then( result => {
+   if (result && result.status === 201)
+      setCharacters([...characters, person] );
+   });
   }
 
   async function fetchAll() {
     try {
-      const response = await axios.get('http://localhost:5000/users');
+      const response = await axios.get('http://localhost:8000/users');
       return response.data.users_list;
     }
     catch (error) {
@@ -34,13 +34,15 @@ function MyApp() {
     }
   }
 
-  async function makePostCall(person) {
-    const response = await axios.post('http://localhost:500/users', person);
-    return response;
-  }
-  catch (error) {
-    console.log(error);
-    return false;
+  async function makePostCall(person){
+   try {
+      const response = await axios.post('http://localhost:8000/users', person);
+      return response;
+   }
+   catch (error) {
+      console.log(error);
+      return false;
+   }
   }
 
   useEffect(() => {
